ae192c44ed08eb01ac5927a8b52d6212979f636f
Dark Boi <66882633+darkdarcool@users.noreply.github.com>
2021-05-04 15:56:39 -0700

    haha e

--- src/main.c
+++ src/main.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
-int main() {
-	char input[100] = "";
-  gets(input);
-	printf("Below is input\n");  
-	
+#include <string.h>
+int main()
+{
+    char str1[100];
+    char input[10][10]; 
+    int i,j,ctr; 
+    printf("$) ");
+    fgets(str1, sizeof str1, stdin);	
+ 
+    j=0; ctr=0;
+    for(i=0;i<=(strlen(str1));i++)
+    {
+        // if space or NULL found, assign NULL into newString[ctr]
+        if(str1[i]==' '||str1[i]=='\0')
+        {
+            input[ctr][j]='\0';
+            ctr++;  //for next word
+            j=0;    //for next word, init index to 0
+        }
+        else
+        {
+            input[ctr][j]=str1[i];
+            j++;
+        }
+    }
+		
+
+    printf(input);
+    return 0;
 }
