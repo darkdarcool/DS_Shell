e976492f027b7aa27506f018bedbe80286a0e5fd
Dark Boi <66882633+darkdarcool@users.noreply.github.com>
2021-05-07 09:08:59 -0700

    python!
    python is plain better because it doesn't annoy me!

---
+++ _read.py
@@ -0,0 +1,25 @@
+import os
+def read(data):
+	e = len(data)
+	for i in range(e):
+		isFile = open("main.py")
+		isFile.close()
+		try:
+			if (data[i] == "read"):
+				f = i + 1
+				if (data[f] == "" or data[f] == " "):
+					print("Expected file but got none")
+					return None
+				try:
+					isFile = open(f"{data[f]}", 'r')
+				
+				except:
+					isFile.close()
+					print(f"{data[f]} is not an existing file")
+					return None
+				isFile.close()
+				os.system("cat " + data[f])
+			else:
+				pass
+		except:
+			pass
---
+++ main.py
@@ -0,0 +1,5 @@
+from _read import read
+while (True):
+	l_data = input("$) ")
+	l_data = l_data.split(" ")
+	read(l_data)
